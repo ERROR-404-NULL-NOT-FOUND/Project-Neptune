602384cae0f1c4f8226d64698be44f17af94cfff
ERROR-404-NULL-NOT-FOUND <giggabyte6@gmail.com>
2021-03-15 18:42:02 -0500

    added text channel leveling

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import discord
 import random
 import datetime
 import json
-from discord.ext import commands
-from discord_slash import SlashCommand
+import os
+#import time
+os.chdir(r'C:\Users\gigga.PAIGE\PycharmProjects\pythonProject')
 
 intents = discord.Intents.all()
 intents.members = True
+from discord.ext import commands
+from discord_slash import SlashCommand
 tokenFile = open('token.tk', 'r')
 token = tokenFile.read()
 prefix = ['ERROR ', 'error ']
@@ -17,10 +20,40 @@ openChannel = 0
 guild_ids = [739204995433496656, 771497539748233257, 729649553648910398, 788789142343122945]
 print(guild_ids)
 
+
 @client.event
 async def on_command_error(ctx, error):
     print(error)
     await ctx.send(error)
+
+
+'''@client.event
+async def on_voice_state_update(member, before, after):
+    print(f'{member} has joined the vc~!')
+    with open('usersVC.json','r') as f:
+            users = json.load(f)
+            id = member.id
+            if str(member.id) not in users:
+                users[str(member.id)] = {}
+                users[str(member.id)]['exp'] = 0
+                users[str(member.id)]['level'] = 1
+            while member.voice is not None:
+                await add_exp(users, member, 1)
+
+                time.sleep(60)
+    print(f'{member} left the vc!')
+    with open('usersVC.json','w') as w:
+        print(f'{member} left the vc!')
+        json.dump(users, w)'''
+
+
+@client.event
+async def on_member_join(member):
+    with open('usersText.json', 'r') as f:
+        users = json.load(f)
+        await update_data(users, member)
+    with open('usersText.json','w') as f:
+        json.dump(users, f)
 
 
 @client.event
@@ -35,6 +68,43 @@ async def remove_reactions(ctx, message):
     message = await ctx.channel.fetch_message(message)
     for r in message.reactions:
         await message.clear_reaction(r)
+
+
+@client.command(description="Returns the level of the given user(if none then it defaults to whoever sends the command")
+async def level(ctx, member: discord.member = None):
+    with open('levelAllowed.json', 'r') as f:
+        guilds = json.load(f)
+        if guilds[str(ctx.guild.id)]["allowed"] == False:
+            return
+    if member is None:
+        member = ctx.author
+    with open('usersText.json', 'r') as f:
+        users = json.load(f)
+        percent = str(users[member.display_name + member.discriminator]["exp"] ** (1 / 4)).split('.')
+        visual = ''
+        percent = percent[1]
+        acPercent = percent[0]+percent[1]
+        percent = percent[0]
+        print(percent)
+        for i in range(int(percent)):
+            visual += '■'
+        for i in range(10-int(percent)):
+            visual+='□'
+        embed = discord.Embed(title=f'Data for {member.display_name}', description=visual+' '+acPercent+'%', color=member.color)
+        embed.add_field(name='EXP', value=str(users[member.display_name+member.discriminator]["exp"]), inline=True)
+        embed.add_field(name='Level', value=str(users[member.display_name+member.discriminator]["level"]), inline=True)
+        await ctx.send(embed=embed)
+
+
+
+@client.command()
+async def toggleTextLevel(ctx, onoroff: bool):
+    with open('levelAllowed.json', 'r') as f:
+        guilds = json.load(f)
+        guilds[str(ctx.guild.id)]['allowed'] = onoroff
+    with open('levelAllowed.json', 'w') as f:
+        json.dump(guilds,f)
+
 
 
 @client.command(description="Returns pong, then bot edits the message to include the client latency")
@@ -187,7 +257,7 @@ async def _user(ctx, member: discord.Member = None):
     await ctx.send(embed=embed)
 
 
-@client.command("for ERROR only")
+@client.command(desc="for ERROR only", hidden=True)
 async def disable_all(ctx):
     if ctx.author.id == 716469153812447233:
         exit(0)
@@ -271,71 +341,36 @@ async def list_users(ctx):
     await ctx.send(embed=embed)
 
 
-@client.command(description="Creates an embed with a message prompt(Warning: does not work)")
-async def embed_long(ctx):
-    embed = discord.Embed()
-    await ctx.send('what do you want the title to be?')
-    title = await client.wait_for('message')
-    embed.title = title.content
-    await ctx.send('what do you want the description to be?')
-    desc = await client.wait_for('message')
-    embed.description = desc.content
-
-    await ctx.send("what do you want the color to be?[int value]")
-    color = await client.wait_for('message')
-    embed.colour = int(color.content)
-
-    await ctx.send("do you want to create a field?")
-    input1 = await client.wait_for('message')
-    create_field = str(input1.content).lower() == 'yes'
-    while create_field:
-        await ctx.send('what do you want the name to be?')
-        name = await client.wait_for('message')
-
-        await ctx.send("Do you want the text to be inline? [true or false]")
-        inline = await client.wait_for('message')
-
-        await ctx.send("what do you want the value to be")
-        value = await client.wait_for('message')
-
-        embed.add_field(name=name.content, inline=inline.content, value=value.content)
-        await ctx.send("do you want to create a field?")
-        input1 = await client.wait_for('message')
-        create_field = str(input1.content).lower() == 'yes'
+@slash.slash(name="embedlong", guild_ids=guild_ids, description="Creates an embed with a message prompt(Warning: does not work)")
+async def _embedlong(ctx, title, desc=None, color=None, fieldName=None, fieldValue=None, fieldInline=True):
+    embed = discord.Embed(title=title, description=desc, color=int(color))
+    embed.add_field(name=fieldName, value=fieldValue, inline=bool(fieldInline))
     await ctx.send(embed=embed)
 
 
 @slash.slash(name='embedshort', guild_ids=guild_ids,description="Creates and sends an embed with a title and description specified by the user")
-async def _embedshort(ctx,*,args):
-    info = args.split("|")
-    desc = info[1]
-    title = info[0]
+async def _embedshort(ctx, title, desc='.'):
     embed = discord.Embed(description=desc, title=title, color=ctx.author.color)
     await ctx.send(embed=embed)
 
 
 @client.event
 async def on_message(message):
+    with open('levelAllowed.json', 'r') as f:
+        guilds=json.load(f)
+        contains = await search(guilds, str(message.guild.id))
+        if not contains:
+            with open('levelAllowed.json', 'w') as w:
+                guilds[message.guild.id] = {}
+                guilds[message.guild.id]['allowed'] = True
+                json.dump(guilds, w)
     if message.author == client.user:
         return
-    log_channel = client.get_channel(795468867794763780)
-    '''global channelLast
-    lastChannel = channelLast
-    lastChannel = client.get_channel(lastChannel)
-    if message.channel == logChannel:
-        await lastChannel.send(message.content)
-        return'''
 
     author = message.author.name
     content = message.content
     channel = message.channel
-    color = message.author.color
-    desc = f'From: <@!{message.author.id}>'
     server = str(message.guild.name)
-    if not message.guild:
-        server = ''
-        embed = discord.Embed(title=f'{message.content[19:]}', description=desc, color=color)
-        await client.get_channel(int(message.content[:19])).send(embed=embed)
 
     print(f'''
     Server: {server}
@@ -344,13 +379,46 @@ async def on_message(message):
                 Message: {content}
 ''')
 
-    embed = discord.Embed(description="", title=f'{message.content}', color=message.author.color)
-    embed.add_field(name=author, value=f'{channel}, {server}', inline=True)
-    embed.set_thumbnail(url=message.author.avatar_url)
-    await log_channel.send(embed=embed)
-    '''channelLast = int(message.channel)'''
+    if not message.author.bot:
+        await client.process_commands(message)
+        with open('levelAllowed.json', 'r') as f:
+            guilds = json.load(f)
+            if guilds[str(message.guild.id)]["allowed"] == False:
+                return
+        with open('usersText.json', 'r') as f:
+            users = json.load(f)
+            await update_data(users, message.author)
+            await add_exp(users, message.author, 5)
+            await level_up(users, message.author, channel)
+        with open('usersText.json','w') as f:
+            json.dump(users, f)
 
-    await client.process_commands(message)
+
+async def update_data(users, user):
+    if str(user.display_name+user.discriminator) not in users:
+        users[str(user.display_name+user.discriminator)] = {}
+        users[str(user.display_name+user.discriminator)]['exp'] = 0
+        users[str(user.display_name+user.discriminator)]['level'] = 1
 
 
+async def add_exp(users, user, exp):
+    users[user.display_name+user.discriminator]['exp'] += exp
+
+
+async def level_up(users, user, channel):
+    exp = users[str(user.display_name+user.discriminator)]['exp']
+    lvl_start = users[str(user.display_name+user.discriminator)]['level']
+    lvl_end = int(exp ** (1/4))
+
+    if lvl_start < lvl_end:
+        await channel.send(f'{user.mention} has leveled up to level {lvl_end}!')
+        users[str(user.display_name+user.discriminator)]['level'] = lvl_end
+    with open('usersText.json','w') as f:
+        json.dump(users, f)
+
+async def search(values, searchFor):
+    for k in values:
+        if searchFor == k:
+            return True
+    return False
 client.run(token)
